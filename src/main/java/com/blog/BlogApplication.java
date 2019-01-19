a1ab79dc66cb3de0cf6e777003bea148c11e3e06
石光 <shiguang@sigon.me>
2019-01-19 21:28:27 +0800

    把内置tomcat去掉,换成单独tomcat方式

--- src/main/java/com/blog/BlogApplication.java
+++ src/main/java/com/blog/BlogApplication.java
@@ -3,16 +3,23 @@ package com.blog;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
 @MapperScan("com.blog.mapper")
 @ComponentScan(basePackages = "com.blog.*")
-public class BlogApplication {
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class BlogApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BlogApplication.class);
+	}
 }
 
